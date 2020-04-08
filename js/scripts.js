85d7c37631eb6a93c9ff189bd35b4d132c35992d
nitunwa <nitun.2@gmail.com>
2020-04-08 11:10:58 -0700

    debug scripts.js, add prototype roll

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // business logic
+var newPlayer1 ;
+var newPlayer1 ;
 function Player(name) {
    this.name = name,
    this.score = 0;
@@ -10,7 +12,11 @@ function Game(player1, player2){
    this.currentPlayer 
    this.winner
 }
-
+Player.prototype.roll1 = function(){
+   var randomDice = Math.floor((Math.random() *6))+1;
+   console.log(randomDice);
+   this.score += randomDice;
+}
 
 
 // user interface logic
@@ -21,13 +27,16 @@ $(document).ready(function(){
       var name1 = $("#player1").val();
       var name2 = $("#player2").val();
       console.log(player1 + " " + player2);
-      var newPlayer1 = new Player(name1);
-      var newPlayer2 = new Player(name2);
-      var newGame = new Game(newPlayer1, newPlayer2);
+      newPlayer1 = new Player(name1);
+       newPlayer2 = new Player(name2);
+       newGame = new Game(newPlayer1, newPlayer2);
       console.log(newPlayer1);
       console.log(newPlayer2);
       console.log(newGame);
     });   
-   
+   $("#play").click(function() {
+      
+      newPlayer1.roll();
+   })
 });
 
